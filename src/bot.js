c2edafa5c3e66cd972346d84cd1aeea1b638e2e3
Caleb Sharp <epicsharp11@gmail.com>
2018-10-24 08:47:22 -0700

    Add punishment

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,36 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const warningList = []
+
+const bannedPhrases = ['😂']
+
+const punishedRole = '504680985867190274'
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+const filterMessage = msg => {
+  bannedPhrases.map(phrase => {
+    if (msg.content.includes(phrase)) {
+      if (warningList.includes(msg.author)) {
+        msg.reply('Again?')
+      } else {
+        msg.reply(`You have been punished for saying '${phrase}'`)
+        warningList.push(msg.author)
+        msg.member.addRole(punishedRole)
+      }
+    }
+  })
+}
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+  if (msg.author.bot) {
+    return
   }
+
+  filterMessage(msg)
 })
 
 module.exports = client
