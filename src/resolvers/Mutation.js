70176bfd740526e68c5b636a1c8be2d23a0c3ca6
Caleb Sharp <epicsharp11@gmail.com>
2018-10-30 11:36:00 -0700

    Add some queries

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,21 @@ const { forwardTo } = require('prisma-binding')
 
 const Mutations = {
   createBannedPhrase: forwardTo('db'),
-  updateBannedPhrase: forwardTo('db')
+  updateBannedPhrase: forwardTo('db'),
+  deleteBannedPhrase: forwardTo('db'),
+  async updatePermissions(parent, args, ctx, info) {
+    return ctx.db.mutation.updateUser(
+      {
+        where: { id: args.id },
+        data: {
+          permissions: {
+            set: args.permissions
+          }
+        }
+      },
+      info
+    )
+  }
 }
 
 module.exports = Mutations
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding')
 
 const Query = {
   bannedPhrases: forwardTo('db'),
-  users: forwardTo('db')
+  users: forwardTo('db'),
+  user: forwardTo('db')
 }
 
 module.exports = Query
